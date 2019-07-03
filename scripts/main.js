1fcf89b21d810d14eae60afe939bfa1eb4806e56
alocsin <41352004+alocsin@users.noreply.github.com>
2019-07-03 15:26:26 -0400

    new jquery script for image hover

--- scripts/main.js
+++ scripts/main.js
@@ -45,3 +45,34 @@ $(document).ready(function() {
     }
   });
 });
+
+$(".text-hover").mouseenter(function() {
+  if (
+    $(this)
+      .parent("div")
+      .children("div.image").length
+  ) {
+    $(this)
+      .parent("div")
+      .children("div.image")
+      .show();
+  } else {
+    var image_name = $(this).data("image");
+    var imageTag =
+      '<div class="image" style="position:absolute; top:0px; left: 465px;">' +
+      '<img src="' +
+      image_name +
+      '" alt="image" height="400" />' +
+      "</div>";
+    $(this)
+      .parent("div")
+      .append(imageTag);
+  }
+});
+
+$(".text-hover").mouseleave(function() {
+  $(this)
+    .parent("div")
+    .children("div.image")
+    .hide();
+});
